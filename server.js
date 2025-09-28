4bcd0f05efd5540876c4bbe3f67033f8e84a9d8f
Sean Kirubi <seannganga17@gmail.com>
2025-09-28 03:37:32 +0300

    refactor server.js to improve API endpoint logic and add settings.json for Postman configuration

--- server.js
+++ server.js
@@ -6,27 +6,29 @@ const app = express();
 //setup static folder
 //app.use(express.static(path.join(__dirname, "public")));
 let posts = [
-  {id: 1, title: "Post one"},
-  {id: 2, title: "Post two"},
-  {id: 3, title: "Post three"},
-  {id: 4, title: "Post four"}
-]
+  { id: 1, title: "Post one" },
+  { id: 2, title: "Post two" },
+  { id: 3, title: "Post three" },
+  { id: 4, title: "Post four" },
+];
 
-app.get('/api/posts', (req, res) => {
+app.get("/api/posts", (req, res) => {
   const limit = parseInt(req.query.limit);
-  console.log("hello world")
-  if(!isNaN(limit) && limit > 0){
-    res.json(posts.slice(0, limit));
-  }else{
+  console.log("hello world");
+  if (!isNaN(limit) && limit > 0) {
+    return res.json(posts.slice(0, limit));
+  }
   res.json(posts);
-}
-})
+});
 
 //get a specific post:
 app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id)
-  const post = posts.filter((post) => post.id === id);
-  res.json(post)
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (post) {
+    return res.status(200).json(post);
+  }
+  res.status(404).json({ message: `post of id ${id} does not exist.` });
 });
 
 app.listen(port, () => console.log(`Server Listening on port: ${port}`));
